8ee5ce1bdc9348449e13f82da2dbb7eccdb6f2dd
Rūdolfs Agris Stilve <rudolfsagris@gmail.com>
2020-04-11 01:00:16 +0300

    that's what you get for not releasing your locks

--- server/src/actions/looping.rs
+++ server/src/actions/looping.rs
@@ -6,34 +6,42 @@ use crate::room::Room;
 
 pub fn room_maintenance(server: &Server) {
     let rooms_to_purge: Mutex<Vec<u64>> = Mutex::new(vec![]);
-    let rooms = server.rooms.read().unwrap();
-    rooms.par_iter().for_each(|r| {
-        let mut room = r.write().unwrap();
-        if room.clients.lock().unwrap().len() > 0 {
-            room.refresh_time = SystemTime::now();
-        }
-        else {
-            match room.refresh_time.elapsed() {
-                Ok(len) => {
-                    if len > Duration::from_secs(10) {
-                        rooms_to_purge.lock().unwrap().push(room.id);
-                        println!("Marking room {} for removal", room.name)
+    {
+        let rooms = server.rooms.read().unwrap();
+        rooms.par_iter().for_each(|r| {
+            let mut room = r.write().unwrap();
+            if room.clients.lock().unwrap().len() > 0 {
+                room.refresh_time = SystemTime::now();
+            }
+            else {
+                match room.refresh_time.elapsed() {
+                    Ok(len) => {
+                        if len > Duration::from_secs(10) {
+                            rooms_to_purge.lock().unwrap().push(room.id);
+                            println!("Marking room {} for removal", room.name)
+                        }
+                    }
+                    Err(_) => {
+                        // Ignore if shenanigans
                     }
                 }
-                Err(_) => {
-                    // Ignore if shenanigans
-                }
             }
-        }
-    });
+        });
+    }
 
     {
         let purge = rooms_to_purge.lock().unwrap();
-        for i in 0..purge.len() {
-            match server.find_room_id(purge[i]) {
-                Some(r) => server.remove_room(r).ok().unwrap(),
-                None => ()
+        if(purge.len() > 0) {
+            println!("Cleaning up {} rooms...", purge.len());
+            for i in 0..purge.len() {
+                match server.find_room_id(purge[i]) {
+                    Some(r) => {
+                        server.remove_room(r).ok().unwrap();
+                    },
+                    None => println!("Error happened when deleting the room :(")
+                }
             }
+            println!("Done cleaning up rooms!");
         }
     }
 }
